510aa378884af81bc49ddd44a50ad3f896c91e68
Rehneet11 <rstudy156@gmail.com>
2024-09-07 00:40:22 +0530

    lol

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const formatPrice = (price: number) => {
-  const formatter = new Intl.NumberFormat('en-US', {
+  const formatter = new Intl.NumberFormat('en-IN', {
     style: 'currency',
-    currency: 'USD',
+    currency: 'INR',
   })
 
   return formatter.format(price)
@@ -42,6 +42,6 @@ export function constructMetadata({
       creator: '@joshtriedcoding',
     },
     icons,
-    metadataBase: new URL("https://casecobra.vercel.app/")
+    metadataBase: new URL("https://casecobra.in/")
   }
 }
